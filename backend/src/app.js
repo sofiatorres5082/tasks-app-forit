b9392b554fcc4179f94fa7f1b064d7de6c73476b
sofiatorres25 <sofiatorres5082@gmail.com>
2025-07-21 22:15:02 -0300

    config: configurar cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,8 +1,13 @@
 import express from 'express';
+import cors from 'cors';
 import taskRoutes from './routes/task.routes.js';
 import { errorHandler } from './middlewares/error.handler.js';
 
 const app = express();
+
+app.use(cors({
+  origin: 'http://localhost:5173' 
+}));
 
 app.use(express.json());
 
